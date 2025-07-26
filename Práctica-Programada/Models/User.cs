a97d3995bd0b3e4f26e7a50f9f00012a6ebd4e09
Harlyn Luna <harlynlunab@gmail.com>
2025-07-25 22:04:20 -0600

    Módulo de Autenticación - Vista preliminar del módulo de Comentarios
    Lo desarrollado en el módulo de autenticación:
    
    - Registro de usuarios con validaciones.
    - Login.
    - Logout.
    - Redirección automática al Login al iniciar el proyecto.
    - Después del login se redirige a la vista de Comentarios.
    
    Desarrollo preliminar del módulo Comentarios:
    
    - Controlador con protección de sesión.
    - Listado de comentarios básico asociados a cada usuario.
    - Formulario básico para crear nuevos comentarios.
    - Botón de Cerrar sesión temporal desde la vista de Comentarios.

---
+++ Práctica-Programada/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Práctica_Programada.Models
+{
+    public class User
+    {
+        // Atributo: Id de usuario
+        public int Id { get; set; }
+
+        // Atributo: Nombre de usuario - Mensaje de validación
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [StringLength(50, ErrorMessage = "Máximo de caracteres 50")]
+        public string Name { get; set; }
+
+        // Atributo: Apellido de usuario - Mensaje de validación
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [StringLength(50, ErrorMessage = "Máximo de caracteres 50")]
+        public string Lastname { get; set; }
+
+        // Atributo: Correo de usuario - Mensaje de validación
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [EmailAddress(ErrorMessage = "Formato de correo no válido")]
+        public string Email { get; set; }
+
+        // Atributo: Contraseña de usuario - Mensaje de validación
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
+        public string Password { get; set; }
+    }
+}
